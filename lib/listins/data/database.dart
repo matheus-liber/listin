0bd5d02e969aeef40b7eff4739363b86d563996c
matheus-liber <matheuslibergt@gmail.com>
2024-06-18 15:56:09 -0300

    Creating database

--- lib/listins/data/database.dart
+++ lib/listins/data/database.dart
@@ -1,4 +1,13 @@
+import "dart:io";
+
 import "package:drift/drift.dart";
+import "package:drift/native.dart";
+import "package:flutter_listin/listins/models/listin.dart";
+import "package:path_provider/path_provider.dart";
+import "package:path/path.dart" as path;
+import "package:sqlite3_flutter_libs/sqlite3_flutter_libs.dart";
+
+part 'database.g.dart';
 
 class ListinTable extends Table {
   IntColumn get id => integer().named('id').autoIncrement()();
@@ -6,4 +15,36 @@ class ListinTable extends Table {
   TextColumn get obs => text().named('obs')();
   DateTimeColumn get dateCreate => dateTime().named('dateCreate')();
   DateTimeColumn get dateUpdate => dateTime().named('dateUpdate')();
+}
+
+@DriftDatabase(tables: [ListinTable])
+class AppDatabase extends _$AppDatabase {
+  AppDatabase() : super(_openConnection());
+
+  @override
+  int get schemaVersion => 1;
+
+  Future<int> insertListin(Listin listin) async {
+    ListinTableCompanion novaLinha = ListinTableCompanion(
+      name: Value(listin.name),
+      obs: Value(listin.obs),
+      dateCreate: Value(listin.dateCreate),
+      dateUpdate: Value(listin.dateUpdate)
+    );
+    return await into(listinTable).insert(novaLinha);
+  }
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(path.join(dbFolder.path, "db.sqlite"));
+
+// Also work around limitations on old Android versions
+    if (Platform.isAndroid) {
+      await applyWorkaroundToOpenSqlite3OnOldAndroidVersions();
+    }
+
+    return NativeDatabase.createInBackground(file);
+  });
 }
